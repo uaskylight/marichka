daba22b921d456287539f1a5b1f9c61d98050d2c
AlexT <uaskylight@gmail.com>
2022-11-10 21:29:58 +0200

    css

---
+++ js/js-speaker-form.js
@@ -0,0 +1,15 @@
+<script>
+      (() => {
+        document
+          .querySelector('.js-speaker-form')
+          .addEventListener('submit', e => {
+            e.preventDefault();
+
+            new FormData(e.currentTarget).forEach((value, name) =>
+              console.log(`${name}: ${value}`),
+            );
+
+            e.currentTarget.reset();
+          );
+      })();
+    </script>
